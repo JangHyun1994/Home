39c6d0dc46012acffbb4bca92601c83060377eee
JangHyun1994 <janghyun0506@gmail.com>
2020-07-12 21:43:10 +0900

    2020-07-12-v1

---
+++ src/Practice_200712.java
@@ -0,0 +1,25 @@
+public class Practice_200712 {
+     
+    public static void main(String[] args) {
+         
+        for(int i = 1; i <= 9; i++) {
+             
+            for(int j=1; j <= 9; j++) {
+                 
+                System.out.println(i + " * " + j + " = " + i * j);
+                 
+            }
+             
+            System.out.println();
+        }
+    }
+}
+
+
+public class Practice_200712 {
+
+	public static void main(String[] args) {
+		
+		for(int i=1; i<=9; )
+	}
+}
